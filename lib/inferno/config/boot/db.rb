4cd7ab16a47a1e4feb6126fca463ee0618298384
Stephen MacVicar <smacvicar@mitre.org>
2021-12-21 10:51:25 -0500

    add retrying to connect to db

--- lib/inferno/config/boot/db.rb
+++ lib/inferno/config/boot/db.rb
@@ -10,8 +10,18 @@ Inferno::Application.boot(:db) do
 
     config_path = File.expand_path('database.yml', File.join(Dir.pwd, 'config'))
     config = YAML.load_file(config_path)[ENV['APP_ENV']]
-      .merge(logger: Inferno::Application['logger'])
-    connection = Sequel.connect(config)
+               .merge(logger: Inferno::Application['logger'])
+    connection = nil
+    5.times do |i|
+      begin
+        connection = Sequel.connect(config)
+        break
+      rescue StardardError => e
+        Inferno::Application['logger'].error(e.full_message)
+        Inferno::Application['logger'].error("Failed to connect to the database #{i + 1}/5")
+        sleep (i + 1) * 2
+      end
+    end
     connection.sql_log_level = :debug
 
     register('db.config', config)
